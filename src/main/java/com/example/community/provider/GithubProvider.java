3aa2e916c07a5d3f0e1fde428c884d59ba10486e
firo <ganles@163.com>
2020-10-08 00:02:16 +0800

    user为空

--- src/main/java/com/example/community/provider/GithubProvider.java
+++ src/main/java/com/example/community/provider/GithubProvider.java
@@ -14,7 +14,7 @@ public class GithubProvider  {
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
@@ -22,25 +22,30 @@ public class GithubProvider  {
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
             String token = string.split("&")[0].split("=")[1];
-            System.out.println(string);
+            System.out.println(token);
             return token;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token="+accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
             String string = response.body().string();
             GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            System.out.println(githubUser);
             return githubUser;
-        } catch (IOException e) {
+        } catch (Exception e) {
             return null;
         }
+
     }
+
 }
+
