dbffd2f36f75feaf8547f8c003c3af9c03b6077b
firo <ganles@163.com>
2020-10-08 12:21:15 +0800

    将数据存入数据库，通过token查询cookie，来实现登录

--- src/main/java/com/example/community/mapper/UserMapper.java
+++ src/main/java/com/example/community/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package com.example.community.mapper;
 import com.example.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 
 @Mapper
 public interface UserMapper {
     @Insert("Insert into user (name,account_Id,token,gmt_Create,gmt_Modified) values (#{name},#{account_Id},#{token},#{gmt_Create},#{gmt_Modified})")
     void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
